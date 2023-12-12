871e1c847c5aabfe69e81befaef4d8f9a4d05940
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-12-12 10:41:26 -0800

    start project and remove item function

---
+++ index.js
@@ -0,0 +1,19 @@
+var drawer = [
+    { name: 'penny', value: 1, quantity: 2 },
+    { name: 'nickel', value: 5, quantity: 0 },
+    { name: 'dime', value: 10, quantity: 0 },
+    { name: 'quarter', value: 25, quantity: 3 },
+    { name: 'one', value: 100, quantity: 2 },
+    { name: 'five', value: 500, quantity: 1 },
+    { name: 'ten', value: 1000, quantity: 1 }
+];
+function removeItem(name, drawer) {
+    for (var i = 0; i < drawer.length; i++) {
+        var obj = drawer[i];
+        if (obj["name"] == name) {
+            obj["quantity"] -= 1;
+        }
+    }
+    return drawer;
+}
+console.log(removeItem("penny", drawer));
